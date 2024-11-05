9db5270ed368b87e278263a3bd86fc5b1164c972
Agus Muhammad Ramdan <agus.ramdan@smartdata-solusi.com>
2024-11-05 17:19:10 +0700

    add amortization index json in Loan Info

--- src/main/java/com/agus/ramdan/loan/domain/LoanInfo.java
+++ src/main/java/com/agus/ramdan/loan/domain/LoanInfo.java
@@ -20,40 +20,45 @@ public class LoanInfo {
     @Column(columnDefinition = "serial")
     @JsonProperty(index = 1)
     private Long id;
-
     @Column(name = "amortization")
-    @JsonProperty(value = "amortization", index = 5)
-    @Schema(description = "value of PAT, FLAT, SLIDING, RC FLAT, RC,FLAT LINKAGE,PPBT,FLAT INSTALLMENT,FLAT POKOK SEKALIGUS,FLAT POKOK BUNGA SEKALIGUS")
+    @JsonProperty(value = "amortization", index = 2)
+    @Schema(description = "amortization formula have value PAT, FLAT, SLIDING, RC FLAT, RC,FLAT LINKAGE,PPBT,FLAT INSTALLMENT,FLAT POKOK SEKALIGUS,FLAT POKOK BUNGA SEKALIGUS")
     private String amortization;
 
     // tenor OR number of month installment
     @Column(name = "tenor_months")
-    @JsonProperty(value = "tenor_months", index = 2)
+    @JsonProperty(value = "tenor_months", index = 3)
+    @Schema
     private Integer tenorMonths;
 
     // percen interest rate per annum
     @Column(name = "interest_rate", precision = 8, scale = 4, nullable = false)
-    @JsonProperty(value = "interest_rate", index = 3)
+    @JsonProperty(value = "interest_rate", index = 4)
+    @Schema
     private BigDecimal interestRate;
 
     // loan amount
     @Column(name = "amount")
-    @JsonProperty(value = "amount", index = 4)
+    @JsonProperty(value = "amount", index = 10)
+    @Schema
     private BigDecimal amount;
 
     //1. Start-of-Period
     //2. In-Arrear / Bayar Bln Berikut
     @Schema(description = "value of In-Arrear / Bayar Bln Berikut,  In-Advance / Bayar Dimuka")
     @Column(name = "payment_method")
-    @JsonProperty(value = "payment_method", index = 5)
+    @JsonProperty(value = "payment_method", index = 11)
     private String paymentMethod;
 
     @Column(name = "disbursement_date")
-    @JsonProperty(value = "disbursement_date", index = 6)
+    @JsonProperty(value = "disbursement_date", index = 12)
+    @Schema
     private LocalDate disbursementDate;
 
     @Column(name = "maturity_date")
-    @JsonProperty(value = "maturity_date", index = 7)
+    @JsonProperty(value = "maturity_date", index = 13)
+    @Schema
     private LocalDate maturityDate;
+
 
 }
