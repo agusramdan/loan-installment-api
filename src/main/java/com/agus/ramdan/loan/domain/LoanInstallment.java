6cb07cde95916b6682f72b7e2d0ca1005514058c
Agus Muhammad Ramdan <agus.ramdan@smartdata-solusi.com>
2024-11-05 13:30:31 +0700

    add field for paid installment and principal balance

--- src/main/java/com/agus/ramdan/loan/domain/LoanInstallment.java
+++ src/main/java/com/agus/ramdan/loan/domain/LoanInstallment.java
@@ -16,31 +16,40 @@ import java.time.LocalDate;
 @Schema
 public class LoanInstallment {
 
-    //    @Column(name = "deleted", nullable = false)
-    //    protected Boolean deleted = false;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(columnDefinition = "serial")
     @JsonProperty(index = 1)
     private Long id;
 
+    @Column(name = "paid_instalment", precision = 12, scale = 2, nullable = false)
+    @Schema(description = "Paid instalment is principal + interest ", example = "100.00", required = true)
+    @JsonProperty(value = "paid_instalment", index = 7)
+    protected BigDecimal paidInstalment;
+
     @Column(name = "number", nullable = false)
     @Schema(description = "Number Installment", example = "1", required = true)
     @JsonProperty(index = 3)
     protected Integer number;
+
     @Column(name = "maturity_date", nullable = false)
     @Schema(required = true)
     @JsonProperty(value = "maturity_date", index = 4)
     protected LocalDate maturityDate;
+
     @Column(name = "principal", precision = 12, scale = 2, nullable = false)
     @Schema(example = "10000.00", required = true)
     @JsonProperty(index = 5)
     protected BigDecimal principal;
+
     @Column(name = "interest", precision = 12, scale = 2, nullable = false)
     @Schema(example = "100.00", required = true)
     @JsonProperty(index = 6)
     protected BigDecimal interest;
+    @Column(name = "principal_balance", precision = 12, scale = 2, nullable = false)
+    @Schema(description = "Last Balance principal ", example = "100.00", required = true)
+    @JsonProperty(value = "principal_balance", index = 8)
+    protected BigDecimal principalBalance;
     @Column(name = "info_id")
     @JsonProperty(value = "info_id", index = 2)
     @Schema(name = "info_id", example = "1", required = true)
